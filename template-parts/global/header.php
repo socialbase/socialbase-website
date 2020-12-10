c58e015cc651fde95b29af3a011a02086daa950b
Leonardo Almeida <lewalmeida@icloud.com>
2020-12-10 17:00:05 -0300

    fix

--- template-parts/global/header.php
+++ template-parts/global/header.php
@@ -76,45 +76,19 @@
     <link rel="stylesheet" href="<?= ASSETS_URI; ?>/layout/css/components/components.min.css?v=<?= VERSION; ?>" />
     <link rel="stylesheet" href="<?= ASSETS_URI; ?>/layout/css/main.min.css?v=<?= VERSION; ?>" />
 
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-28923705-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-28923705-1');
-    </script>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PFQQJCD" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-
-    <!-- Hotjar Tracking Code for https://socialbase.com.br -->
-    <script>
-      (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:1409883,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
-    <!-- Hotjar Tracking Code for https://socialbase.com.br -->
-
-    <!-- Intercom -->
-    <script>
-      window.intercomSettings = {
-        app_id: "b3graaa5"
-      };
-    </script>
-    <script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/b3graaa5';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
-    <!-- Intercom -->
-
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PVR88JB');</script>
+    <!-- End Google Tag Manager -->
   </head>
   <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PVR88JB"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
     <?php require('navbar.php'); ?>
 
--- template-parts/login/footer.php
+++ template-parts/login/footer.php
@@ -9,7 +9,5 @@
     <script src="<?= ASSETS_URI; ?>/plugins/jquery/jquery-3.5.1.min.js?v=<?= VERSION; ?>"></script>
     <script src="<?= ASSETS_URI; ?>/plugins/bootstrap-4.5.2/js/bootstrap.min.js?v=<?= VERSION; ?>"></script>
     <script src="<?= ASSETS_URI; ?>/plugins/jquery/jquery.validate.min.js?v=<?= VERSION; ?>"></script>
-    <script src="<?= ASSETS_URI; ?>/layout/js/login.js?v=<?= VERSION; ?>"></script>
-
   </body>
 </html>
