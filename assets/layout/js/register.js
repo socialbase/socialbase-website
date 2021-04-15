db4c1f7ecf31d9a76a36202201722b4ee45554b9
Leonardo Almeida <lewalmeida@icloud.com>
2021-04-15 03:18:17 -0300

    fix

--- assets/layout/js/register.js
+++ assets/layout/js/register.js
@@ -25,8 +25,10 @@ var RegisterSB = ( function () {
 
       phone.bind( 'paste', function (e) { e.preventDefault(); } );
 
-      var SPMaskBehavior = function (val) { return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '00 (00) 00000-0000'; },
-        spOptions = {
+      var SPMaskBehavior = function (val) {
+        return val.replace(/\D/g, '').length === 11 ? '(00) 00000-000099' : '00 (00) 00000-0000';
+      },
+      spOptions = {
         onKeyPress : function (val, e, field, options) {
           field.mask(SPMaskBehavior.apply({}, arguments), options);
         },
